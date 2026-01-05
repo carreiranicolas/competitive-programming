662a0f5170806c48fb5e5e7d5f84717f088122ad
carreiranicolas <nicolascarreira2005@gmail.com>
2026-01-04 22:41:27 -0300

    Fiz um exercicio facil

---
+++ Kattis/kattis-13floors.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int x;
+
+	cin >> x;
+
+	if(x < 13) {
+		cout << x;
+	}
+	else {
+		cout << x + 1;
+	}
+
+	return 0;
+}
